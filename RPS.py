34beb8cdb368005b4dd1ebfcbe48eef16ef7ca83
Connor Adams <cleeadacon909@gmail.com>
2022-06-27 15:18:26 -0700

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -14,28 +14,38 @@ def player(prev_play, opponent_history = [], response=[], my_history=[]):
 
     if len(opponent_history) > 10:
         global df, X, y, test_pred_decision_tree
+        # This is a dictionary for accessing the proper response based on the result of the decision tree.
         dic = {0: 'P', 1: 'R', 2: 'S'}
-        response.append(prev_play)
-        df = pd.DataFrame({'New Move': response, 'History': opponent_history, 'My History': my_history})
-        df = df.iloc[1:]
-        dftest = pd.DataFrame({'History': [prev_play], 'My History': my_history.pop()})
+
+        response.append(prev_play) # Creates a response list of the opponents move to my move
+
+        df = pd.DataFrame({'New Move': response, 'History': opponent_history, 'My History': my_history}) # Creates the
+                                            # dataframe such that the opponents recent move is the response variable.
+        df = df.iloc[1:] # Removes the first row because the previous move of the first row is blank.
+        dftest = pd.DataFrame({'History': [prev_play], 'My History': my_history.pop()}) # This is my test set.
+
+        # This is where I try to make the variables categorical, but it isn't working well
         X_df = df.drop(columns='New Move')
         y_df = df['New Move']
         X_df = X_df.apply(LabelEncoder().fit_transform)
         X = OneHotEncoder(sparse=False).fit_transform(X_df.values)
-        X = X.shape
         y = LabelEncoder().fit_transform(y_df.values)
         y = y.reshape(-1, 1)
         xTest = dftest.apply(LabelEncoder().fit_transform)
         xTest = OneHotEncoder(sparse=False).fit_transform(xTest.values)
-        xTest = xTest.shape
-        opponent_history.append(prev_play)
+
+        opponent_history.append(prev_play) # Stores the opponents recent move in their history of moves list
+
+        # Beginning of the decision tree model.
         X_train, test_x, y_train, test_lab = train_test_split(X, y,test_size=0.4,random_state=42)
         clf = DecisionTreeClassifier(max_depth=3,random_state=42)
         clf.fit(X_train,y_train)
         test_pred_decision_tree = clf.predict(xTest)
-        # player_choice = dic.get(test_pred_decision_tree[0])
-        my_history.append(player_choice)
+
+        player_choice = dic.get(test_pred_decision_tree[0]) # This is where I access the dictionary to figure out the
+                                                            # opponents next move.
+
+        my_history.append(player_choice) # This stores the history of my moves
         return player_choice
     elif len(opponent_history)>0:
         response.append(prev_play)
@@ -53,6 +63,7 @@ def player(prev_play, opponent_history = [], response=[], my_history=[]):
 
 play(player,quincy,3000)
 
+display(df)
 
 
 
